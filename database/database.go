316a6353311c47c3f897026491e3d33ba651740a
unflorhuang <unflorhuang@tencent.com>
2025-08-18 16:21:30 +0800

    go-查看留言&查看评论

--- database/database.go
+++ database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
 )
 
@@ -11,7 +12,9 @@ var err error
 
 func init() {
 	dsn := "root:root@tcp(localhost:3306)/tb_demo?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		log.Println(err)
 	}
